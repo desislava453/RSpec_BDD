cd5b064131c723e564d9d002778dab42b3ad5573
desislava453 <desislava453@gmail.com>
2022-10-27 15:17:19 +0300

    added chrome options

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,11 @@ require 'yaml'
 
 RSpec.configure do |config|
   config.before(:each) do
-    @driver = Selenium::WebDriver.for :chrome
+    chrome_options = Selenium::WebDriver::Chrome::Options.new
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu') 
+    @driver = Selenium::WebDriver.for :chrome, :options => chrome_options
     @driver.manage.window.maximize
     env_data = YAML.load_file('config/env.yml')
     @driver.get(env_data[:url])
